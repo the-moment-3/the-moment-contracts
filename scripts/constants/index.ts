85f5e6f359aa93f8cd782dcb0362076b8a27cfc9
mfer725 <cxw.crypto@gmail.com>
2023-06-20 13:18:27 +0800

    feat: update contractAddress

--- scripts/constants/index.ts
+++ scripts/constants/index.ts
@@ -1,2 +1,2 @@
-export const contractAddress = "0x420718143440a327b128348eB539E3ec1d7Db64f";
+export const contractAddress = "0x054F7B885E153f2DCE7fC88d4adcE7A9f12029Dc";
 export const userAddress = "0xd41392CdDfCc3052E564D0dC040D0400CEA0a842"; // dev wallet
